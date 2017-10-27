a2c40a29eee85bfacbfa732aa8dbb1a465089906
sharils <sharils@users.noreply.github.com>
2017-10-27 22:02:52 +0800

    Use rg for fzf

--- plugins/fzf.plugins.bash
+++ plugins/fzf.plugins.bash
@@ -2,3 +2,9 @@
 
 # You can add these options to $FZF_DEFAULT_OPTS so that they're applied by default. For example,
 export FZF_DEFAULT_OPTS='--multi'
+
+# Setting rg as the default source for fzf
+export FZF_DEFAULT_COMMAND='rg --files'
+
+# To apply the command to CTRL-T as well
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
