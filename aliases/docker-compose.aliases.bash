4b6675314f682c56832fba38077639b43358f185
sharils <sharils@users.noreply.github.com>
2017-12-02 18:31:45 +0800

    Make consistent

--- aliases/docker-compose.aliases.bash
+++ aliases/docker-compose.aliases.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-alias dcrr="docker-compose run --rm --volume $PWD:$PWD --workdir $PWD"
-alias dcru="docker-compose run --rm --user $(id -u):$(id -g) --volume $PWD:$PWD --workdir $PWD"
+alias dcorr="docker-compose run --rm --volume $PWD:$PWD --workdir $PWD"
+alias dcoru="docker-compose run --rm --user $(id -u):$(id -g) --volume $PWD:$PWD --workdir $PWD"
