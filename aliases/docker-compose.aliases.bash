29291985c9cabc51cfd9b079c93992b0cca08c21
sharils <sharils@users.noreply.github.com>
2017-12-07 21:17:59 +0800

    Alias dco up

--- aliases/docker-compose.aliases.bash
+++ aliases/docker-compose.aliases.bash
@@ -3,4 +3,5 @@
 alias dcod="docker-compose down"
 alias dcorr="docker-compose run --rm --volume $PWD:$PWD --workdir $PWD"
 alias dcoru="docker-compose run --rm --user $(id -u):$(id -g) --volume $PWD:$PWD --workdir $PWD"
+alias dcou="docker-compose up"
 alias dcoud="docker-compose up -d"
