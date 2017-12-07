a82c8809bcca87099033b5a1054fd2497a734e18
sharils <sharils@users.noreply.github.com>
2017-12-07 21:08:30 +0800

    Add docker-compose up -d

--- aliases/docker-compose.aliases.bash
+++ aliases/docker-compose.aliases.bash
@@ -2,3 +2,4 @@
 
 alias dcorr="docker-compose run --rm --volume $PWD:$PWD --workdir $PWD"
 alias dcoru="docker-compose run --rm --user $(id -u):$(id -g) --volume $PWD:$PWD --workdir $PWD"
+alias dcoud="docker-compose up -d"
