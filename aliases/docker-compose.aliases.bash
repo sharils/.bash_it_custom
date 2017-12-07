de931e8b0265bc436cd7d69f6f8563428d8e17f6
sharils <sharils@users.noreply.github.com>
2017-12-07 21:16:31 +0800

    Add docker-compose down

--- aliases/docker-compose.aliases.bash
+++ aliases/docker-compose.aliases.bash
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+alias dcod="docker-compose down"
 alias dcorr="docker-compose run --rm --volume $PWD:$PWD --workdir $PWD"
 alias dcoru="docker-compose run --rm --user $(id -u):$(id -g) --volume $PWD:$PWD --workdir $PWD"
 alias dcoud="docker-compose up -d"
